a764051ad5a571c2900becd637fbf9939c2a16dc
kesavaprasad08 <kesav2661998@gmail.com>
2023-04-24 02:02:44 +0530

    Added-Cart

---
+++ src/Components/Ui/Modal.js
@@ -0,0 +1,31 @@
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <civ className={classes.content}>{props.children}</civ>
+    </div>
+  );
+};
+const portalElement = document.getElementById("overlays");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
